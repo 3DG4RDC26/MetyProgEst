2c05c0299eb733521a4eb431b1851d953189c10d
3DG4RDC26 <ercasco@uamv.edu.ni>
2024-08-23 00:46:01 -0600

    Sesion1

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Suma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma;
+            Console.WriteLine("Digite el primer numero");
+            num1 = int.Parse(Console.ReadLine());
+            num2= int.Parse(Console.ReadLine());
+
+            Suma = num1 + num2
+            Console.WriteLine("Digite el segundio numero: " + suma);
+        }
+    }
+}
