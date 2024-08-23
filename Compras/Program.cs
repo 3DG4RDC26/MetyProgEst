58ab99237d0936d086c9c81862b02b09fcc4ce37
3DG4RDC26 <ercasco@uamv.edu.ni>
2024-08-23 01:27:49 -0600

    Sesion2

---
+++ Compras/Program.cs
@@ -0,0 +1,13 @@
+﻿using Compras.clases;
+
+namespace Compras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 1");
+            Factura fact = new Factura();
+        }
+    }
+}
